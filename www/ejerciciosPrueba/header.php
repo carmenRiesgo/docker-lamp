f5f55570e62d196142a32d5065973276af0b72e5
Carmen Riesgo <a22mariarl@iessanclemente.net>
2024-10-22 00:12:14 +0200

    primer conmit ejercicio 1 tarea UD02

---
+++ www/ejerciciosPrueba/header.php
@@ -0,0 +1,15 @@
+
+#Apartado 1 tarea UD02
+<!DOCTYPE>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>><?php echo "UD02 - CARMEN RIESGO"?></title>
+    </head>
+    <body>
+        <header class="bg-primary text-white text-center py-3">
+        <h1><?php echo "UD02 - CARMEN RIESGO";?></h1>   
+        <h2><?php echo "tarea UD02 - Creación cabecera con título";?></h2> 
+    </body>
+</html>
