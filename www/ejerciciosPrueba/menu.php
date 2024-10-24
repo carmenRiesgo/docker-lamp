13b249b956b0de80a0ca2d358bb8cab272dfe6a7
Carmen Riesgo <a22mariarl@iessanclemente.net>
2024-10-24 19:38:50 +0200

    grabación apartado 2 y 3 de la tarea

---
+++ www/ejerciciosPrueba/menu.php
@@ -0,0 +1,24 @@
+
+<nav class="col-md-3 col-lg-2 d-md-block bg-light sidebar">
+<div class="position-sticky">
+    <ul class="nav flex-column">
+        <li class="nav-item">
+            <a class="nav-link" href="index.php" alt="Enlace a menú principal">
+                Home
+            </a>
+        </li>
+        <li>
+            <a class="nav-link" href="listaTareas.php" alt="Enlace a listar tareas">
+                Mis tareas
+            </a>
+        </li>
+        <li>
+            <a class="nav-link" href="nuevaForm.php" alt="Enlace a menú principal">
+                Nueva tarea
+            </a>
+        </li>
+    </ul>
+</div>
+</nav>
+
+    
